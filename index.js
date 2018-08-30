86062513ad3fb97f28614a2a4056099504ced5d7
gianna.jolibeth15@gmail.com <gianna.jolibeth15@gmail.com>
2018-08-30 23:00:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,5 +25,7 @@ function sayHiToGrandma(string)
   else if (string.toUpperCase() === string){
     return "YES INDEED!"
   }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
   //Check if what's coming in is equal to what you would've expected if you had lower cased the string 
 }
