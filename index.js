6e844a82e83e4bbd05ee0ab0666803eb4d531d3d
gianna.jolibeth15@gmail.com <gianna.jolibeth15@gmail.com>
2018-08-30 22:39:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+var output
+function shout(string)
+{
+  return string.toUpperCase()
+}
+function whisper(string)
+{
+  return string.toLowerCase()
+}
+function logShout(string)
+{
+output = string.toUpperCase()
+console.log(output)
+}
+function logWhisper(string)
+{
+  output = string.toLowerCase()
+  console.log(output)
+}
+var uppercase = "HELLO!"
+uppercase.toUpperCase() === uppercase
+
+function ()
